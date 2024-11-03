103a5adb75162d00856814a698dda52e91ae0ffa
Kagrtavy <nesterivan97@gmail.com>
2024-11-03 12:54:02 +0200

    forms init

---
+++ app/views/common/aut_form.php
@@ -0,0 +1,12 @@
+<form method="post" action="">
+    <label>
+        Enter your login: <input type="text" name="login" required>
+    </label>
+    <label>
+        Enter your email: <input type="email" name="email" required>
+    </label>
+    <label>
+        Enter your password: <input type="password" name="password" required>
+    </label>
+    <input type="submit" value="Submit">
+</form>
---
+++ app/views/common/notes_form.php
@@ -0,0 +1,6 @@
+<form method="post" action="">
+    <label>
+        Enter your note: <textarea name="note" rows="4" cols="50"></textarea>
+    </label>
+    <input type="submit" value="Save">
+</form>
